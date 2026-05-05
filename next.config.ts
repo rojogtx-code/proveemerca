f297bb35f71fd84cb0ebce8479083b33b0186d95
AgusPC <agus@example.com>
2026-05-05 15:15:29 -0600

    revert: removed basePath to use default Vercel domain

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/actualizacion",
-  trailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
