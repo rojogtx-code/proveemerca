639bb613382bc9b9a5df14d99cbe974cb9a6f5a6
AgusPC <agus@example.com>
2026-05-05 14:35:22 -0600

    feat: added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-white text-slate-900">
         {children}
+        <Analytics />
       </body>
     </html>
   );
